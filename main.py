e3a048905325385e5b336fa881cb7124dd117a34
Markus <kremer.markus1996@gmail.com>
2024-06-29 23:32:51 +0200

    add upload; minor stuff

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import logging
 from datetime import datetime
-from os import path
+from os import path, getenv
+from ftplib import FTP
 
 import dateutil.parser
 import requests
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
 from dateutil import tz
 
 
@@ -27,14 +28,11 @@ def main():
     )
 
     html = BeautifulSoup(res.text, "html.parser")
-    container = html.select_one("main div.pt-4:not(.row)")
 
-    if not isinstance(container, Tag):
-        raise ValueError(f"Container is not a Tag: {type(container)}")
+    if not (container := html.select_one("main div.pt-4:not(.row)")):
+        raise ValueError("Container not found.")
 
-    items = container.select(".card:not(.native-ad-story) .boxlink")
-
-    if not items:
+    if not (items := container.select(".card:not(.native-ad-story) .boxlink")):
         raise ValueError("No items found in container.")
 
     testberichte = []
@@ -43,7 +41,7 @@ def main():
         link = f"https://www.1000ps.de{item.get('href', '')}"
 
         img = item.select_one(".card-img img")
-        img_src = img.get("data-src") if img else ""  # getattr doesn't work here
+        img_src = img.get("data-src") if img else ""
 
         content = item.select_one(".card-body .card-text")
 
@@ -72,26 +70,18 @@ def main():
                 continue
 
         if img_src:
-            content.append(
-                BeautifulSoup(f"<img src='{img_src}' />", features="html.parser")
-            )
+            content.append(BeautifulSoup(f"<img src='{img_src}' />", features="html.parser"))
 
         testberichte.append(
             Testbericht(
                 title,
                 link,
                 content.prettify(),
-                dateutil.parser.parse(date_str, fuzzy=True),  # dateutil <3
+                dateutil.parser.parse(date_str, fuzzy=True),
             )
         )
 
-    feed = generate_feed(testberichte)
-    with open(
-        path.join(path.dirname(path.realpath(__file__)), "1000ps.rss"),
-        "w",
-        encoding="utf-8",
-    ) as f:
-        f.write(BeautifulSoup(feed, "xml").prettify())
+    Feed(testberichte).save().upload()
 
 
 class Testbericht:
@@ -99,9 +89,7 @@ class Testbericht:
         self.title: str = title
         self.link: str = link
         self.description: str = description
-        self.pub_date: str = pub_date.replace(tzinfo=tz.UTC).strftime(
-            "%a, %d %b %Y %H:%M:%S %z"
-        )
+        self.pub_date: str = pub_date.replace(tzinfo=tz.UTC).strftime("%a, %d %b %Y %H:%M:%S %z")
 
     def generate_xml(self) -> str:
         return f"""
@@ -114,20 +102,61 @@ class Testbericht:
         """
 
 
-def generate_feed(testberichte: list[Testbericht]) -> str:
-    items = "".join([testbericht.generate_xml() for testbericht in testberichte])
+class Feed:
+    def __init__(self, testberichte: list[Testbericht]):
+        self.testberichte = testberichte
+        self.file_path = path.join(path.dirname(path.realpath(__file__)), "1000ps.rss")
 
-    return f"""
-    <rss version="2.0">
-      <channel>
-        <title>1000PS Testberichte</title>
-        <link>https://www.1000ps.de/motorrad-testberichte</link>
-        <description>Inoffizieller Feed für die neuesten Testberichte von 1000PS</description>
-        <lastBuildDate>{datetime.now().replace(tzinfo=tz.tzlocal()).strftime("%a, %d %b %Y %H:%M:%S %z")}</lastBuildDate>
-        {items}
-      </channel>
-    </rss>
-    """
+    def create(self) -> str:
+        return f"""
+        <rss version="2.0">
+        <channel>
+            <title>1000PS Testberichte</title>
+            <link>https://www.1000ps.de/motorrad-testberichte</link>
+            <description>Inoffizieller Feed für die neuesten Testberichte von 1000PS</description>
+            <lastBuildDate>
+                {datetime.now().replace(tzinfo=tz.tzlocal()).strftime("%a, %d %b %Y %H:%M:%S %z")}
+            </lastBuildDate>
+            {"".join([testbericht.generate_xml() for testbericht in self.testberichte])}
+        </channel>
+        </rss>
+        """
+
+    def save(self) -> "Feed":
+        with open(
+            self.file_path,
+            "w",
+            encoding="utf-8",
+        ) as file:
+            file.write(BeautifulSoup(self.create(), "xml").prettify())
+        return self
+
+    def upload(self) -> None:
+        ftp_host = getenv("FTP_HOST")
+        ftp_user = getenv("FTP_USER")
+        ftp_pass = getenv("FTP_PASS")
+        ftp_path = getenv("FTP_PATH")
+
+        if not ftp_host:
+            return
+
+        if not ftp_user or not ftp_pass:
+            raise ValueError("FTP_USER and FTP_PASS must be set in the .env file.")
+
+        ftp = FTP(ftp_host)
+        ftp.login(ftp_user, ftp_pass)
+
+        if ftp_path:
+            ftp.cwd(ftp_path)
+
+        with open(self.file_path, "rb") as file:
+            ftp.storbinary("STOR 1000ps.rss", file)
+            if ftp.lastresp == "226":
+                print("Upload successful.")
+            else:
+                logging.error("Error uploading: %s", ftp.lastresp)
+
+        ftp.quit()
 
 
 if __name__ == "__main__":
